b191f0c8b7288ff98a0e54020e47b249ca3fcb15
F74021200 <gzxc3kc4690@gmail.com>
2019-03-03 19:31:45 +0800

    Produce rand-No. array
    In merge-sort.c,this program first produces an array with random
    numbers in the same way as insert-sort.c.However, the merge-sort
    function isn't written.

---
+++ examples/merge-sort.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "list.h"
+
+static uint16_t values[256];
+
+int main()
+{
+    struct list_head testlist;
+    struct listitem *item = NULL;
+    size_t i;
+
+    random_shuffle_array(values, (uint16_t) ARRAY_SIZE(values));
+
+    INIT_LIST_HEAD(&testlist);
+
+    assert(list_empty(&testlist));
+
+    for (i = 0; i < ARRAY_SIZE(values); i++) {
+        item = (struct listitem *) malloc(sizeof(*item));
+        assert(item);
+        item->i = values[i];
+        list_add_tail(&item->list, &testlist);
+    }
+    list_for_each_entry (item, &testlist, list) {
+        printf("%d ", item->i);
+    }
+    printf("\n");
+
+    return 0;
+}
