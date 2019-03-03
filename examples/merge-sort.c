86259cc0d9b8896fb5e99053dc7e05b320e16e9f
F74021200 <gzxc3kc4690@gmail.com>
2019-03-03 20:00:05 +0800

    Compare and merge two lists
    In merge-sort,two lists will compared and merged many times, so using
    a function to implement the two action can make the codes more
    readible.list_1 and list_2 is two lists to be merged.The two lists are
    sorted in non-decreasing order.In each iteration,delete the node with
    the smallest number in the two lists, and add the node into tmp_list
    from its tail.When either of two lists is empty, add the the other into
    tmp_list from its tail.tmp_list is what splited lists are merged into.

--- examples/merge-sort.c
+++ examples/merge-sort.c
@@ -7,6 +7,26 @@
 
 static uint16_t values[256];
 
+void cmp_merge(struct list_head *tmp_list,
+               struct list_head *list_1,
+               struct list_head *list_2)
+{
+    struct listitem *item_1 = NULL, *item_2 = NULL;
+
+    while (!list_empty(list_1) && !list_empty(list_2)) {
+        item_1 = list_entry(list_1->next, struct listitem, list);
+        item_2 = list_entry(list_2->next, struct listitem, list);
+        if (item_1->i <= item_2->i)
+            list_move_tail(list_1->next, tmp_list);
+        else
+            list_move_tail(list_2->next, tmp_list);
+    }
+    if (list_empty(list_1))
+        list_splice_tail_init(list_2, tmp_list);
+    else
+        list_splice_tail_init(list_1, tmp_list);
+}
+
 void merge_sort(struct list_head *unsorted_list)
 {
     struct listitem *item = NULL;
